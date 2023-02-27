3de0cfcd6301d657ac9a360cf9e9d434882bb086
tobby8629 <sarhmullah15@gmail.com>
2023-02-27 07:09:46 -0800

    clear linter errors

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import store from './redux/app/store';
- 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
